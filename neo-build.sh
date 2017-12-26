1faa073c4470790bd55f5643d3efee578690f81e
thirdzcee <ironbuang@gmail.com>
2017-12-26 23:12:02 +0800

    add neocore buildscript

---
+++ neo-build.sh
@@ -0,0 +1,46 @@
+export KBUILD_BUILD_USER=thirdzcee
+export ARCH=arm64
+export CROSS_COMPILE=/home/ironbuang/android/toolchains/aarch64-gcc-linaro-7.1.1-2017.05/bin/aarch64-linux-gnu-
+
+DIR=$(pwd)
+BUILD="$DIR/build"
+OUT="$DIR/zip"
+ZIPNAME="neocore.zip"
+NPR=`expr $(nproc) + 1`
+
+echo "cleaning build..."
+if [ -d "$BUILD" ]; then
+rm -rf "$BUILD"
+fi
+if [ -d "$OUT" ]; then
+rm -rf "$OUT/modules"
+rm -rf "$OUT/Image.gz-dtb"
+fi
+
+echo "setting up build..."
+mkdir "$BUILD"
+make O="$BUILD" h990_neo_defconfig
+
+echo "building kernel..."
+time make O="$BUILD" -j$NPR 2>&1 |tee ../compile.log
+
+echo "building modules..."
+make O="$BUILD" INSTALL_MOD_PATH="." INSTALL_MOD_STRIP=1 modules_install
+rm $BUILD/lib/modules/*/build
+rm $BUILD/lib/modules/*/source
+
+mkdir -p $OUT/modules
+mv "$BUILD/arch/arm64/boot/Image.gz-dtb" "$OUT/Image.gz-dtb"
+find "$BUILD/lib/modules/" -name *.ko | xargs -n 1 -I '{}' mv {} "$OUT/modules"
+cd zip
+#mv modules/exfat.ko modules/texfat.ko
+zip -q -r "$ZIPNAME" anykernel.sh META-INF tools modules Image.gz-dtb setfiles.conf ramdisk patch
+
+mv "$ZIPNAME" "/home/ironbuang/android/h990/$ZIPNAME"
+
+rm -rf "$OUT/modules"
+rm -rf "$OUT/Image.gz-dtb"
+
+echo "Done !"
+
+
