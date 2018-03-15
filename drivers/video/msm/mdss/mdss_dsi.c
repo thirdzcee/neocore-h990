e1551fd7444627c69830705f880e3f6b88a43398
xNombre <kartapolska@gmail.com>
2018-03-15 15:24:30 +0100

    state_notifier: add hooks and increase delay
    Without hooks state_notifier is useless :D
    Delay set to 5s should be fine.

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -27,6 +27,10 @@
 #include <linux/msm-bus.h>
 #include <linux/pm_qos.h>
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
 #include "mdss.h"
 #include "mdss_panel.h"
 #include "mdss_dsi.h"
@@ -447,6 +451,9 @@ static int mdss_dsi_panel_power_ctrl(struct mdss_panel_data *pdata,
 		else
 #endif
 			ret = mdss_dsi_panel_power_off(pdata);
+#ifdef CONFIG_STATE_NOTIFIER
+		state_suspend();
+#endif
 		break;
 	case MDSS_PANEL_POWER_ON:
 		if (mdss_dsi_is_panel_on_lp(pdata))
@@ -461,6 +468,9 @@ static int mdss_dsi_panel_power_ctrl(struct mdss_panel_data *pdata,
 			else
 #endif
 				ret = mdss_dsi_panel_power_on(pdata);
+#ifdef CONFIG_STATE_NOTIFIER
+		state_resume();
+#endif
 		break;
 	case MDSS_PANEL_POWER_LP1:
 	case MDSS_PANEL_POWER_LP2:
