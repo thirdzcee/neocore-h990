6142997d16a247f1d439c8b666a5445a2960a145
Dorimanx <yuri@bynet.co.il>
2018-04-24 18:44:39 +0300

    patch-3.18.104-105

--- net/core/neighbour.c
+++ net/core/neighbour.c
@@ -1156,10 +1156,6 @@ int neigh_update(struct neighbour *neigh, const u8 *lladdr, u8 new,
 		lladdr = neigh->ha;
 	}
 
-	if (new & NUD_CONNECTED)
-		neigh->confirmed = jiffies;
-	neigh->updated = jiffies;
-
 	/* If entry was valid and address is not changed,
 	   do not change entry state, if new one is STALE.
 	 */
@@ -1181,6 +1177,16 @@ int neigh_update(struct neighbour *neigh, const u8 *lladdr, u8 new,
 			    )
 				new = old;
 		}
+	}
+
+	/* Update timestamps only once we know we will make a change to the
+	 * neighbour entry. Otherwise we risk to move the locktime window with
+	 * noop updates and ignore relevant ARP updates.
+	 */
+	if (new != old || lladdr != neigh->ha) {
+		if (new & NUD_CONNECTED)
+			neigh->confirmed = jiffies;
+		neigh->updated = jiffies;
 	}
 
 	if (new != old) {
